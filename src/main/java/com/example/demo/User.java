11f47f7ea2a08042a5e2d402773df8c210ddb147
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-21 02:27:21 -0400

    initial commit

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Collection;
 
 @Entity
@@ -12,21 +16,31 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @NotNull
+    @Email
     @Column(name="email", nullable = false)
     private String email;
 
+    @NotNull
+    @Size(min=3)
     @Column(name="password")
     private String password;
 
+    @NotNull
+    @Size(min=3)
     @Column(name="first_name")
     private String firstName;
 
+    @NotNull
+    @Size(min=3)
     @Column(name="last_name")
     private String lastName;
 
     @Column(name = "enabled")
     private boolean enabled;
 
+    @NotNull
+    @Size(min=3)
     @Column(name="username")
     private String username;
 
