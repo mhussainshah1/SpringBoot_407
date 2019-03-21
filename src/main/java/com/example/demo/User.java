d51ecbab72ddc21f1388c06224eefa48320aed72
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-21 03:04:08 -0400

    initial commit

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -4,7 +4,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Collection;
